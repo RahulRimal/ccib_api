f955b2ec56caf60504c8a5fa72d501d5080513e3
Arjun Dhami <arjundhami185@gmail.com>
2024-05-15 16:22:26 +0000

    added user admin panel

--- autho/admin.py
+++ autho/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from autho.models import User
+
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "idx",
+        "username",
+        "first_name",
+        "last_name",
+        "citizenship_number",
+        "citizenship_issued_place",
+        "citizenship_issued_date",
+        "dob",
+        "fathers_name",
+        "phone_number",
+    ]
