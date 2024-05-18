857f88692031a4167c1b3dd4658fea95dec7197a
Krishna Rimal <krishna.rimal@khalti.com>
2024-05-18 06:48:37 +0000

    refactored User model

--- autho/models.py
+++ autho/models.py
@@ -1,18 +1,104 @@
-from django.db import models
+import datetime
 
-# Create your models here.
-from django.contrib.auth.models import User as BaseUser
+from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager as BaseUserManager
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 from common.models import BaseModelMixin
 
 
-class User(BaseUser, BaseModelMixin):
+class UserManager(BaseUserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        extra_fields.setdefault("citizenship_number", "")
+        extra_fields.setdefault("citizenship_issued_place", "")
+        extra_fields.setdefault("citizenship_issued_date", datetime.date.today())
+        extra_fields.setdefault("father_name", "")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModelMixin):
+    """
+    Username and password are required. Other fields are optional.
+    """
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
     middle_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), blank=True)
     citizenship_number = models.CharField(max_length=50)
     citizenship_issued_place = models.CharField(max_length=255)
     citizenship_issued_date = models.DateField()
     dob = models.DateField(blank=True, null=True)
-    fathers_name = models.CharField(max_length=255)
+    father_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    
     def __str__(self):
         return self.first_name
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
