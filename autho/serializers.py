6ccae7a67ee0ae9910c7900052d67592a983b1fe
Arjun Dhami <arjundhami185@gmail.com>
2024-05-19 08:15:19 +0000

    remove create and update

--- autho/serializers.py
+++ autho/serializers.py
@@ -31,57 +31,11 @@ class UserSerializer(BaseModelSerializerMixin):
         validated_data["username"] = username
         return super().create(validated_data)
 
-
-class UserCreateSerializer(BaseModelSerializerMixin):
-    username = serializers.ReadOnlyField()
-
-    class Meta:
-        model = User
-        fields = [
-            "idx",
-            "username",
-            "first_name",
-            "middle_name",
-            "last_name",
-            "citizenship_number",
-            "citizenship_issued_place",
-            "citizenship_issued_date",
-            "dob",
-            "father_name",
-            "phone_number",
-        ]
-
-    def create(self, validated_data):
-        username = generate_username(
-            validated_data["first_name"], validated_data["last_name"]
-        )
-        validated_data["username"] = username
-        return super().create(validated_data)
-
-
-class UserUpdateSerializer(BaseModelSerializerMixin):
-    username = serializers.ReadOnlyField()
-
-    class Meta:
-        model = User
-        fields = [
-            "idx",
-            "username",
-            "first_name",
-            "middle_name",
-            "last_name",
-            "citizenship_number",
-            "citizenship_issued_place",
-            "citizenship_issued_date",
-            "dob",
-            "father_name",
-            "phone_number",
-        ]
-
     def update(self, instance, validated_data):
-        username = generate_username(
-            validated_data.get("first_name", instance.first_name),
-            validated_data.get("last_name", instance.last_name),
-        )
-        validated_data["username"] = username
-        return super().update(instance, validated_data)
+     username = generate_username(
+        validated_data.get("first_name", instance.first_name),
+        validated_data.get("last_name", instance.last_name),
+    )
+     validated_data["username"] = username
+     return super().update(instance, validated_data)
+
