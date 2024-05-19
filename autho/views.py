c899b8c585a132d972a1e6aa37dcdb77e83c62a2
Arjun Dhami <arjundhami185@gmail.com>
2024-05-19 08:02:49 +0000

    added user serializerand view set

--- autho/views.py
+++ autho/views.py
@@ -1,12 +1,20 @@
 from autho.models import User
 from rest_framework.viewsets import ModelViewSet
+from common.mixins import BaseApiMixin
 
-from autho.serializers import UserSerializer
+from autho.serializers import UserCreateSerializer, UserSerializer, UserUpdateSerializer
 
 # Create your views here.
 
 
-class UserViewSet(ModelViewSet):
-
+class UserViewSet(BaseApiMixin, ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete"]
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    # serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return UserCreateSerializer
+        if self.request.method == "PATCH":
+            return UserUpdateSerializer
+        return UserSerializer
