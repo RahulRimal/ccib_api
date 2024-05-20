a4ed60c64b260964c76f9fe5a292c8be25ca6711
Arjun Dhami <arjundhami185@gmail.com>
2024-05-20 09:22:54 +0000

    added the views in token

--- autho/urls.py
+++ autho/urls.py
@@ -1,19 +1,18 @@
 from django.urls import path
-from . import views as autho_views
-from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView
+from . import views 
 
 # from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 router = DefaultRouter()
 
-router.register('users', autho_views.UserViewSet, basename='users')
+router.register('users', views.UserViewSet, basename='users')
 
 
 urlpatterns = [
-    path("create-token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh-token/",TokenRefreshView.as_view(), name="token_refresh"),
-    path("verify-token/", TokenVerifyView.as_view(), name="token_verify"),
+    path("create-token/", views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh-token/",views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify-token/", views.TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 
