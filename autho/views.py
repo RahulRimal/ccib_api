c77f5c0dffbe9374cc4cb65d8b5cf2a6fe127597
Arjun Dhami <arjundhami185@gmail.com>
2024-05-21 08:44:29 +0000

    added change password and the api response in common app

--- autho/views.py
+++ autho/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from common.api_response import api_response_error, api_response_success
 from common.mixins import BaseApiMixin
 from autho.serializers import UserSerializer
 
@@ -24,8 +25,33 @@ class UserViewSet(BaseApiMixin, ModelViewSet):
         user = request.user
         serializer = self.get_serializer(user)
         return Response(serializer.data)
+    
+    
+    @action(detail=False, methods=["POST"])
+    def change_password(self, request: HttpRequest) -> Response:
+        user: User = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
 
+        is_correct = user.check_password(old_password)
+        if is_correct:
+            if not new_password:
+                return api_response_error(
+                    {"detail": "New password cannot be empty."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            user.set_password(new_password)
+            user.save()
+            return api_response_success(
+                {"detail": "Password updated successfully."}, status=status.HTTP_200_OK
+            )
+        else:
+            return api_response_error(
+                {"detail": "Old password is incorrect."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
+  
 
 class TokenObtainPairView(TokenObtainPairView):
     # permission_classes = ([BazraPermission])
---
+++ common/api_response.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+
+def api_response_success(data, status=status.HTTP_200_OK):
+
+    return Response(data, status)
+
+
+def api_response_error(data, status=status.HTTP_400_BAD_REQUEST):
+
+    return Response(data, status)
