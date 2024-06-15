b55064b9c0e2336528690900c26e839a272491e3
Krishna Rimal <rimalkrishna205657@gmail.com>
2024-06-15 12:26:07 +0000

    Binding subscriptions with permissions

--- common/helpers.py
+++ common/helpers.py
@@ -1,10 +1,11 @@
-from autho.models import StaffUser
 from django.utils import timezone
 from datetime import date, datetime
 
 
 def generate_username(first_name: str, last_name: str) -> str:
     """Generate a unique username"""
+    
+    from autho.models import StaffUser
     username = first_name + last_name
     if StaffUser.objects.filter(username=username).exists():
         username = username + str(StaffUser.objects.count() + 1)
